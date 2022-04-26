e2928316ddce051997b12f1101092f56e2f20fc0
Artem Verenik <amaynov@mail.ru>
2022-04-27 00:18:15 +0300

    Threads manipulation, code fixes

--- OS/Lab3 (98)/main.cpp
+++ OS/Lab3 (98)/main.cpp
@@ -18,7 +18,7 @@ DWORD WINAPI markerThread(void* args)
     int id = params->id;
     int nMarkedElements = 0;
     int num;
-    srand(time(0));
+    srand(id);
 
     WaitForSingleObject(hStartAllEvent, INFINITE);
 
@@ -26,21 +26,21 @@ DWORD WINAPI markerThread(void* args)
     {
         num = rand() % n;
         EnterCriticalSection(&csMarkElement);
-        if (arr[num] != 0)
+        if (arr[num] == 0)
         {
+            ++nMarkedElements;
             Sleep(5);
             arr[num] = id;
             Sleep(5);
-
             LeaveCriticalSection(&csMarkElement);
+
         }
         else
         {
-            LeaveCriticalSection(&csMarkElement);
-
             std::cout << "[marker:" << id << "] "
             << "nMarkedElements = " << nMarkedElements
             << "; failed with index " << num << std::endl;
+            LeaveCriticalSection(&csMarkElement);
 
             SetEvent(hDeadlocks[id - 1]);
             DWORD answer = WaitForMultipleObjects(2, hAnswers[id - 1], FALSE, INFINITE);
@@ -59,11 +59,9 @@ DWORD WINAPI markerThread(void* args)
                         if (arr[i] == id) arr[i] = 0;
                     }
                     LeaveCriticalSection(&csMarkElement);
-                    ExitThread(1);
-                    break;
+                    return 1;
                 default:
-                    ExitThread(2);
-                    break;
+                    return 2;
             }
         }
     }
@@ -96,6 +94,7 @@ int main()
     int nArr;
     int* arr;
     threadParams* params;
+    bool* terminatedFlags;
 
     std::cout << "Enter number of threads to create : ";
     std::cin >> totalThreads;
@@ -109,47 +108,94 @@ int main()
         return 0;
     }
 
+
     hThreads = new HANDLE[totalThreads];
     params = new threadParams[totalThreads];
+    terminatedFlags = new bool[totalThreads];
+
+    for (int i = 0; i < nArr; ++i) arr[i] = 0;
+    for (int i = 0; i < totalThreads; ++i) terminatedFlags[i] = false;
+
     if (!hThreads || !params)
     {
         logError("Couldn't allocate resources", "Out of memory!");
     }
 
-    hStartAllEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    hStartAllEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
     for (int i = 1; i <= totalThreads; ++i)
     {
-        params[i] = threadParams(i, arr, nArr);
-        hThreads[i] = CreateThread(
+        params[i - 1] = threadParams(i, arr, nArr);
+        hThreads[i - 1] = CreateThread(
                 NULL,
                 0,
                 markerThread,
-                static_cast<void*>(&params[i]),
+                static_cast<void*>(&params[i - 1]),
                 0,
                 NULL
         );
-        if (!hThreads[i]) logErrorWin32("Creating marker thread");
+        if (!hThreads[i - 1]) logErrorWin32("Creating marker thread");
     }
 
     InitializeCriticalSection(&csMarkElement);
 
     hAnswers = new HANDLE*[totalThreads];
+    hDeadlocks = new HANDLE[totalThreads];
     for (int i = 0; i < totalThreads; i++)
     {
-        hDeadlocks = new HANDLE[totalThreads];
-        hDeadlocks[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        hDeadlocks[i] = CreateEvent(NULL, TRUE, FALSE, NULL);
 
         hAnswers[i] = new HANDLE[2];
         hAnswers[i][0] = CreateEvent(NULL, FALSE, FALSE, NULL);
         hAnswers[i][1] = CreateEvent(NULL, FALSE, FALSE, NULL);
     }
 
+    SetEvent(hStartAllEvent);
     runningThreads = totalThreads;
     while (runningThreads)
     {
-        break;
-        // TODO: thread manipulation
+        WaitForMultipleObjects(totalThreads, hDeadlocks, TRUE, INFINITE);
+        std::cout << "Array : ";
+        outArr(arr, nArr);
+        std::cout << std::endl;
+
+        int idToTerminate;
+        bool terminateOk = false;
+
+        do {
+            std::cout << "Enter thread id to terminate ( [1, totalAmountOfThreads] ) : ";
+            std::cin >> idToTerminate;
+            if (idToTerminate < 1 || idToTerminate > totalThreads)
+            {
+                std::cout << "Incorrect index!" << std::endl;
+            }
+            else if (terminatedFlags[idToTerminate - 1])
+            {
+                std::cout << "Thread is already terminated!" << std::endl;
+            }
+            else
+            {
+                SetEvent(hAnswers[idToTerminate - 1][1]);
+                WaitForSingleObject(hThreads[idToTerminate - 1], INFINITE);
+                terminateOk = true;
+                terminatedFlags[idToTerminate - 1] = true;
+                --runningThreads;
+
+                std::cout << "Array : ";
+                outArr(arr, nArr);
+                std::cout << std::endl;
+            }
+        } while (!terminateOk);
+        for (int i = 0; i < totalThreads; ++i)
+        {
+            if (!terminatedFlags[i])
+            {
+                ResetEvent(hDeadlocks[i]);
+                SetEvent(hAnswers[i][0]);
+            }
+        }
     }
+
+    std::cout << "There are no running threads left..." << std::endl;
 
     DeleteCriticalSection(&csMarkElement);
     cleanHandles(totalThreads);
@@ -157,10 +203,14 @@ int main()
     {
         delete[] hAnswers[i];
     }
+    delete[] terminatedFlags;
     delete[] hAnswers;
     delete[] hThreads;
     delete[] hDeadlocks;
     delete[] params;
     delete[] arr;
+
+    std::cout << "Done cleaning!" << std::endl;
+    system("pause");
     return 0;
 }
